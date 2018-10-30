8a61156c102e2c86c00ce0953c36222df0800bce
 slevenet <Seeattackwin32>
2018-10-31 00:27:28 +0300

    New SUPPORTED_INSTRUMENT is added

--- src/main/java/exchange/analyzer/constants/BasicConstant.java
+++ src/main/java/exchange/analyzer/constants/BasicConstant.java
@@ -5,5 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 final public class BasicConstant {
-    public static final List<String> SUPPORTED_INSTRUMENT = new ArrayList<>(Arrays.asList("EUR_USD"));
+    public static final List<String> SUPPORTED_INSTRUMENT = new ArrayList<>(Arrays.asList("EUR_USD", "AUD_USD","EUR_AUD",
+            "EUR_JPY","GBP_JPY","GBP_USD", "NZD_USD", "USD_CAD", "USD_CHF", "USD_JPY","XAU_USD"));
 }
--- src/main/java/exchange/analyzer/controller/AutochartistRestController.java
+++ src/main/java/exchange/analyzer/controller/AutochartistRestController.java
@@ -1,20 +1,23 @@
 package exchange.analyzer.controller;
 
 import exchange.analyzer.dao.services.AutochartistOperationsService;
+import exchange.analyzer.innerLogics.storages.ChartPatternStorage;
+import exchange.analyzer.model.autochartist.Signal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class AutochartistRestController {
 
     @Autowired
-    AutochartistOperationsService autochartistOperationsService;
+    ChartPatternStorage autochartistOperationsService;
 
     @GetMapping("/autochartist")
-    public List<String> getPatterns(){
-        return null;
+    public Map<Long, Signal>  getPatterns(){
+        return autochartistOperationsService.getPatterns();
     }
 }
