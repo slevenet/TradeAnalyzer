162a36a60e71fe9f8aef3c20aa0081301dcb620d
slevenet <ser.levenetc@gmail.com>
2019-02-07 15:41:29 +0300

    It works

--- src/main/java/exchange/analyzer/entity/CandlePatterns.java
+++ src/main/java/exchange/analyzer/entity/CandlePatterns.java
@@ -4,10 +4,19 @@ import exchange.analyzer.CandlePatternsListner;
 
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.math.BigInteger;
 
 @Entity
 @EntityListeners(CandlePatternsListner.class)
-@Table(name = "")
+@Table(name = "CANDLEPATTERNS")
 public class CandlePatterns {
+
+    @Id
+    private BigInteger id;
+    private String timeStamp;
+    private String tf;
+    private String type;
+    private BigInteger parent_id;
 }
